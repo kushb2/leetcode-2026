994e580d336eb5d25d433643eeb880eed78bd740
kushb2 <kushkkr08@gmail.com>
2026-09-04 17:06:05 +0530

    Time: 22 ms (81.38%) | Memory: 51.8 MB (72.59%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,65 +1,58 @@
-
-
 class LRUCache {
 public:
-    class Node {
-    public:
-        int key, value;
-        Node *prev, *next;
 
-        Node(int k, int v){
-            key = k;
-            value = v;
-            prev = next = NULL;
+    class DoublyLinkList{
+        public:
+        int key, vale;
+        DoublyLinkList *prev, *next;
+
+        DoublyLinkList(int k, int v){
+            key = k, vale = v;
+            prev = next = nullptr;
         }
     };
 
-    void addNode(Node* newNode){
-        // prev   newNode    next
-        Node* prev = head;
-        Node* next = head->next;
+    int limit;
+    DoublyLinkList* head = new DoublyLinkList(-1,-1);
+    DoublyLinkList* tail = new DoublyLinkList(-1,-1);
+    unordered_map<int,DoublyLinkList*> map;
+
+    void addNode(DoublyLinkList* newNode){
+        // prev/head  newNode  next 
+        DoublyLinkList* prev = head;
+        DoublyLinkList* next = head->next;
 
         prev->next = newNode;
-
         newNode->prev = prev;
         newNode->next = next;
-
         next->prev = newNode;
 
     }
 
-    int limt = 0;
-    Node* head = new Node(-1,-1);
-    Node* tail = new Node(-1,-1);
+    void removeNode(DoublyLinkList * node){
+        // prev nodeForDeletion next
+        DoublyLinkList* prev = node->prev;
+        DoublyLinkList* next = node->next;
 
-    unordered_map<int, Node*> map;
-
-    void remove(Node* existingNode){
-        // prev existingNode next
-        Node* prev = existingNode->prev;
-        Node* next = existingNode->next;
         prev->next = next;
         next->prev = prev;
     }
-    
+     // head -> tail
     LRUCache(int capacity) {
-        limt = capacity;
+        limit = capacity;
         head->next = tail;
         tail->prev = head;
     }
     
     int get(int key) {
         if(map.contains(key) == false) return -1;
-
-        Node* existingNode = map[key];
-        int ans = existingNode->value;
-
-        remove(existingNode);
+        int ans = map[key]->vale;
+        removeNode(map[key]);
         map.erase(key);
 
-        Node* newNode = new Node(key, ans);
+        DoublyLinkList *newNode = new DoublyLinkList(key,ans);
         addNode(newNode);
-        map[key] = newNode;
+        map[key]= newNode;
 
         return ans;
         
@@ -67,17 +60,17 @@ public:
     
     void put(int key, int value) {
         if(map.contains(key)){
-            remove(map[key]);
-             map.erase(key);
-        }
-        if(map.size() == limt){
+            removeNode(map[key]);
+            map.erase(key);
+        }else if(map.size() == limit){
             map.erase(tail->prev->key);
-            remove(tail->prev);
+            removeNode(tail->prev);
         }
 
-        Node* newNode = new Node(key,value);
+        DoublyLinkList *newNode = new DoublyLinkList(key,value);
         addNode(newNode);
-        map[key] = newNode;
+        map[key]= newNode;
+        
     }
 };
 
