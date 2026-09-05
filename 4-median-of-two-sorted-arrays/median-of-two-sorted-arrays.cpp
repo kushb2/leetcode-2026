86d6099246ae5dffca6e304fb47e6c3c2f8e2fdb
kushb2 <kushkkr08@gmail.com>
2026-09-05 08:52:55 +0530

    Time: 123 ms (5.78%) | Memory: 95.1 MB (79.75%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-       int n = nums1.size(), m = nums2.size();
-       int totalElement = n + m;
-       int medianIndex = totalElement/2;
-       bool odd = true;
-       if(totalElement%2 == 0){
-            odd = false;
-            medianIndex--;
-       }
-
-       int i=0, j=0, k = 0;
-       double sum = 0;
-       while(i < n || j < m){
-        int x = i < n ? nums1[i] : INT_MAX;
-        int y = j < m ? nums2[j] : INT_MAX;
-        int chooseNum;
-        if( x < y) {
-            chooseNum = x;
-            i++;
-        }else{
-            chooseNum = y;
-            j++;
+        // if odd then middle other wise (middle-1 + middle)/2
+        int n = nums1.size(), m = nums2.size();
+        bool odd = (n + m) % 2 == 0 ? false : true;
+        int middle = (n+m)/2;
+        int i = 0, j = 0, k = 0;
+        double prev = 0, curr = 0;
+        while(k <= middle){
+            int a = i < n ? nums1[i] : INT_MAX;
+            int b = j < m ? nums2[j] : INT_MAX;
+            prev = curr;
+            if(a < b){
+                curr = a;
+                i++;
+            }else{
+                curr = b;
+                j++;
+            }
+            k++;
+            cout<<prev<<" "<<curr<<endl;
         }
 
-        if(medianIndex == k){
-            sum += chooseNum;
-            if(odd) break;
-        }else if(!odd && medianIndex+1 == k){
-            sum += chooseNum;
-            break;
-        }
-        k++;
-       }
+        return odd ? curr : (prev + curr)/2;
 
-       return odd ? sum : sum/2;
         
     }
 };
