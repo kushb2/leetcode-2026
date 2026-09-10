e99e539a8577171b3fbf10ed426b65c5e1751f68
kushb2 <kushkkr08@gmail.com>
2026-09-10 12:12:51 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (44.25%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,16 +4,18 @@ public:
         pair<int,int> possibleMajorityElement;
         possibleMajorityElement = {nums[0], 1};
         for(int i=1;i<nums.size();i++){
+            if(possibleMajorityElement.second == 0){
+                possibleMajorityElement = {nums[i], 1};
+                continue;
+            }
+
             if(nums[i] != possibleMajorityElement.first){
                 possibleMajorityElement.second--;
             }else{
                 possibleMajorityElement.second++;
             }
 
-            if(possibleMajorityElement.second == 0 && i < nums.size()-1){
-                possibleMajorityElement = {nums[i+1], 1};
-                i++;
-            }
+            
         }
         return possibleMajorityElement.first;
         
