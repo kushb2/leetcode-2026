933a11ff5be629bacded616683457e091c5509a1
kushb2 <kushkkr08@gmail.com>
2026-09-13 16:02:53 +0530

    Time: 5 ms (13.02%) | Memory: 43.4 MB (40.80%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        // ()[]{}
+        // every time a closing bracket comes 
+        // stack top should be the same if not return false
+        // if yes pop the element 
+        // handle stack empty case
+        // at end if stack empty return true false 
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        Stack<Character> st = new Stack<>();
+
+        for(char c: s.toCharArray()){
+            if(map.containsKey(c)){// exit brac is there 
+
+                char top = st.isEmpty() ? '*' : st.pop();
+                
+                if(top != map.get(c)) return false;
+
+            }else{
+                st.add(c);
+            }
+            
+        }
+        return st.isEmpty() ? true : false;
+
+    }
+}
