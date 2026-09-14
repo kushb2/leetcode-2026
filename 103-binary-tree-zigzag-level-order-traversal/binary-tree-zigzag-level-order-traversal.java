8dace56e5409658cc613f82a39c8540bbcb87270
kushb2 <kushkkr08@gmail.com>
2026-09-14 14:18:48 +0530

    Time: 1 ms (82.02%) | Memory: 43.9 MB (24.98%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -35,7 +35,7 @@ class Solution {
             }
         }
         if(!leftToRight) {
-            list = list.reversed();
+            Collections.reverse(list);
         }
         leftToRight = !leftToRight;
         ans.add(list); 
