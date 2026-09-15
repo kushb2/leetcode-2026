a79e0c681fb9d9d0c1d44edaf387e4837eb5705c
kushb2 <kushkkr08@gmail.com>
2026-09-15 17:55:58 +0530

    Time: 5 ms (13.00%) | Memory: 43.4 MB (40.76%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,31 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-        // ()[]{}
-        // every time a closing bracket comes 
-        // stack top should be the same if not return false
-        // if yes pop the element 
-        // handle stack empty case
-        // at end if stack empty return true false 
+        // every time a closing bracket come 
+        // check stack top is opening bracket or not 
+        
         Map<Character, Character> map = new HashMap<>();
         map.put(')', '(');
         map.put(']', '[');
         map.put('}', '{');
-
-        Deque<Character> st = new ArrayDeque<>();
-
+        Stack<Character> st = new Stack<>();
         for(char c: s.toCharArray()){
-            if(map.containsKey(c)){// exit brac is there 
-
-                char top = st.isEmpty() ? '*' : st.pop();
-                
-                if(top != map.get(c)) return false;
-
+            if(map.containsKey(c)){
+                Character stackTop = st.isEmpty() ? '*' : st.peek();
+                if(stackTop != map.get(c)) return false;
+                st.pop();
             }else{
                 st.push(c);
             }
-            
         }
         return st.isEmpty();
-
     }
 }
