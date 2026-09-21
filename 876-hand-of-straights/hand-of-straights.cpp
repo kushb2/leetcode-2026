3484350a0593a3263a91a5d20c3d6749cfda38bd
kushb2 <kushkkr08@gmail.com>
2026-09-22 01:13:31 +0530

    Time: 147 ms (5.03%) | Memory: 32.3 MB (64.59%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+       map<int,int> cardAndThereFreq;
+       for(auto it: hand){
+        cardAndThereFreq[it]++;
+       }
+       
+       while(!cardAndThereFreq.empty()){
+            auto [card, freq] = *cardAndThereFreq.begin();
+            int currCard = card;
+             int currGroupSize = 0;
+            while(currGroupSize < groupSize){
+                if(cardAndThereFreq.contains(currCard)){
+                    currGroupSize++;
+                    cardAndThereFreq[currCard]--;
+                    if(cardAndThereFreq[currCard] == 0){
+                        cout<<currCard<<endl;
+                        cardAndThereFreq.erase(currCard);
+                    }
+                    currCard++;
+                }else{
+                    break;
+                }
+            }
+            if(currGroupSize != groupSize) return false;
+        
+        }
+        return true;
+            
+       }
+};
