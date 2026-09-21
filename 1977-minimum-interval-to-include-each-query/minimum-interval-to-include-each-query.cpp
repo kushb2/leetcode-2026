e0b249065ff47197bf8c27011207c234297e2ba3
kushb2 <kushkkr08@gmail.com>
2026-09-21 17:30:48 +0530

    Time: 1808 ms (5.07%) | Memory: 521.2 MB (5.30%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
@@ -18,20 +18,20 @@ public:
 
         // create a min heap 
         priority_queue<
-        pair<int, vector<int>>,
-        vector<pair<int, vector<int>>>,
-        greater<pair<int, vector<int>>>
+        pair<int, int>,
+        vector<pair<int, int>>,
+        greater<pair<int,int>>
         > pq;
         int right = 0;
         vector<int> ans(queries.size());
         for(auto [key,index] : queryList){
             while(right < intervals.size() && intervals[right][0] <= key){ // all valid interval added 
                 // can be valid ans add to the heap 
-                pq.push({intervals[right][1] - intervals[right][0] + 1, intervals[right]});
+                pq.push({intervals[right][1] - intervals[right][0] + 1, intervals[right][1]});
                 right++;
             }
 
-            while(!pq.empty() && pq.top().second[1] < key){ // expired 
+            while(!pq.empty() && pq.top().second < key){ // expired 
                 pq.pop();
             }
 
