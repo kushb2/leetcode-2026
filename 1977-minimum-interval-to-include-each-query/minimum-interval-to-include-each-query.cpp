73bf77498d86fcfc95c003b9663e0998388a8a23
kushb2 <kushkkr08@gmail.com>
2026-09-21 17:29:15 +0530

    Time: 2012 ms (5.07%) | Memory: 540.1 MB (5.30%) - LeetSync

---
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool comp(vector<int> a , vector<int> b){
+        return a[0] < b[0];
+    }
+    vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
+        // first sort the queries 
+        vector<pair<int, int>> queryList;
+        for(int i=0;i<queries.size();i++){
+            queryList.push_back({queries[i], i});
+        }
+        sort(queryList.begin(), queryList.end());
+
+        // sort the interval by start 
+        sort(intervals.begin(), intervals.end(), [](vector<int> a , vector<int> b) {
+             return a[0] < b[0];
+        });
+
+        // create a min heap 
+        priority_queue<
+        pair<int, vector<int>>,
+        vector<pair<int, vector<int>>>,
+        greater<pair<int, vector<int>>>
+        > pq;
+        int right = 0;
+        vector<int> ans(queries.size());
+        for(auto [key,index] : queryList){
+            while(right < intervals.size() && intervals[right][0] <= key){ // all valid interval added 
+                // can be valid ans add to the heap 
+                pq.push({intervals[right][1] - intervals[right][0] + 1, intervals[right]});
+                right++;
+            }
+
+            while(!pq.empty() && pq.top().second[1] < key){ // expired 
+                pq.pop();
+            }
+
+
+            ans[index] = pq.empty() ? -1 : pq.top().first;
+
+        }
+
+        return ans;
+
+       
+    }
+};
