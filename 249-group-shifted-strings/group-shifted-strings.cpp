851e0c423a132bd3e96ad43fd40bd8026a902fef
kushb2 <kushkkr08@gmail.com>
2026-09-22 20:24:47 +0530

    Time: 4 ms (26.96%) | Memory: 12.9 MB (9.80%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.cpp
+++ 249-group-shifted-strings/group-shifted-strings.cpp
@@ -4,10 +4,10 @@ public:
         string converted = "0,";
 
         for(int i=1;i<s.size();i++){
-            int diff = abs(s[i-1] - s[i] + 26) % 26;
+            int diff = (s[i-1] - s[i] + 26) % 26;
             converted += to_string(diff) + ",";
         }
-
+            // 25+26 => 51%26
         return converted;
 
     }
