cf859081753982b186e31a0c672a485daf863f39
kushb2 <kushkkr08@gmail.com>
2026-09-22 13:51:44 +0530

    Time: 321 ms (5.03%) | Memory: 21.7 MB (19.71%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> minStep(n, INT_MAX); // each index i will store minimum step to reach at this index
+
+        minStep[0] = 0;
+        for(int i=1;i<n;i++){
+            // can i reach here from previous index 
+            for(int j=0; j<i;j++){
+                if(j + nums[j] >= i){ // can reach
+                    minStep[i] = min(minStep[i], minStep[j] + 1);
+                    break;
+                }
+            }
+        }
+
+        return minStep[n-1];
+        
+    }
+};
