37dafb6f4f4b3512458a0bdda8f8f4ec0e408b2e
kushb2 <kushkkr08@gmail.com>
2026-09-22 20:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (48.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // what is an anagram every single char of s should apprean in t 
+        // with same frequency if i add s frwq and minum t freq then last freq should be zero
+        if(s.size() != t.size()) return false;
         unordered_map<char, int> map;
-        for(auto it: s) map[it]++;
-        for(auto it: t) map[it]--;
+        for(int i=0;i<s.size();i++){
+            map[s[i]]++;
+            map[t[i]]--;
+        }
 
         for(auto [key, freq]: map){
-            if(freq != 0) return false;
+            if(freq > 0) return false;
         }
         return true;
-
+       
+        
+        
     }
 };
