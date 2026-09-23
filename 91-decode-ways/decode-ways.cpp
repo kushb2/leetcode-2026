5aeeedbc8a7ac5748a99f123641251fe3a1bf417
kushb2 <kushkkr08@gmail.com>
2026-09-23 22:43:46 +0530

    Time: 4 ms (6.40%) | Memory: 11.6 MB (5.89%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        int n = s.size();
-        if( n == 0 || s[0] == '0') return 0; // base case handled 
-        vector<int> dp(n);
-        dp[0] = 1; // will be a valid digit as leading zero alreay handled 
-        if(n == 1) return dp[0];
+    int solve(string s, int n, vector<int> &dp){
 
-        int signleDigit = s[1] - '0';
-        if(signleDigit >= 1 && signleDigit <= 9){
-                dp[1] += dp[0];
-            }
-        int doubleDigit = (s[0] - '0')*10 + (s[1] - '0');  
-          if(doubleDigit >= 10 && doubleDigit <= 26){
-                dp[1] +=1;
-            }
-
-
-        for(int i=2;i<n;i++){
-            // taken as a single char 
-             int oneDigit = s[i] - '0';
-            if(oneDigit >= 1 && oneDigit <= 9){
-                dp[i] = dp[i-1];
-            }
-
-            int combine = (s[i-1] - '0')*10 + (s[i] - '0');
-            if(combine >= 10 && combine <= 26){
-                dp[i] += dp[i-2]; 
-            }
+        if(n == 0){
+            dp[0] = 1;
+            return dp[0];
+            
         }
-        return dp[n-1];
+        if( n < 0) return 1; // 12 leaves nothing as prefic
 
-        
+        if(dp[n] != -1) return dp[n];
+
+        int ans = 0;
+        int oneDigit = s[n] - '0';
+        if( oneDigit >= 1 && oneDigit <= 9){
+            ans += solve(s, n-1, dp);
+        }
+        int twoDigit = (s[n-1] - '0')*10 + (s[n] - '0');// 12
+        if(twoDigit >= 10 && twoDigit <= 26){
+            ans += solve(s, n-2, dp);
+        }
+        dp[n] = ans;
+        return ans;
+    }
+    int numDecodings(string s) {
+        if(s[0] == '0') return 0;
+        vector<int> dp(s.size(), -1);
+        return solve(s, s.size()-1, dp);
     }
 };
