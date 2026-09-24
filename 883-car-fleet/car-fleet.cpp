78ae943f264a7f2bbd6f21fdb56d939e2f7eb8cb
kushb2 <kushkkr08@gmail.com>
2026-09-24 19:23:25 +0530

    Time: 47 ms (53.64%) | Memory: 116.4 MB (20.44%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -2,27 +2,22 @@ class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         int n = position.size();
-        vector<pair<int,int>> arr; // position and speed 
+        vector<pair<int,double>> arr; // position and (speed, time to reach destination) 
         for(int i=0;i<n;i++){
-            arr.push_back({position[i], speed[i]});
+            double timeToReachDestination = (double)(target - position[i] ) / (double)speed[i];// how much time this car to reach at 
+            arr.push_back( {position[i], timeToReachDestination});
         }
         sort(arr.begin(), arr.end()); // all line in a single line sorted by there position
 
-        stack<pair<int,int>> st;
+        stack<pair<int,double>> st;
         for(int i=n-1;i>=0;i--){
-            // can this catch a car or not 
-            double currCarTime = (double)(target - arr[i].first ) / (double)arr[i].second;// how much time this car to reach at 
-            // destination 
-            if(!st.empty()){
-                double advanceCarToReachDestination = (double)(target - st.top().first)/ (double)st.top().second;
-
-                if(currCarTime > advanceCarToReachDestination){
-                    // will not be able to catch 
-                    st.push(arr[i]);
-                }
+            
+            if(!st.empty() && arr[i].second <= st.top().second){
+                // do nothing
             }else{
-                st.push(arr[i]); // last car wait for any car for catchup to become a fleep
+                st.push(arr[i]);
             }
+            
         }
 
         return st.size();
