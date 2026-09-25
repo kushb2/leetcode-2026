39f92623f9067e8d0966e7757223f32bcbb2319a
kushb2 <kushkkr08@gmail.com>
2026-09-25 13:27:09 +0530

    Time: 22 ms (81.69%) | Memory: 60.2 MB (85.53%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size() == 0) return 0; 
+        if(nums.size() == 0) return 0;
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int maxLen = 1;
-        int currLen = 1;
-        for(int i=1;i<n;i++){
-            if(nums[i] != nums[i-1]){
-                if(nums[i] == nums[i-1]+1){
-                    currLen++;
-                }else{
-                    maxLen = max(maxLen, currLen);
-                    currLen = 1;
-                }
+        int ans = 1; // a single interger will satify condition 
+        int count = 1; // local len for a valid substring 
+
+        for(int i=1;i<nums.size();i++){
+            
+            if(nums[i-1] == nums[i]) continue;
+            if(nums[i-1]+1 == nums[i]){ //1 - 2 
+                count++;
+            }else{ 
+                ans = max(ans, count);
+                count = 1;
             }
         }
+        return max(ans, count);
         
-        return max(maxLen, currLen);
     }
 };
-
-// [100,4,200,1,3,2]
-// 1 2 3 4 100 200 
-
-// i < n 
-// len = 1 
-// i+1 < n => len ++
-// max ans 
-// start again 
