50fa6addce951aaeb38ecef217250411790d9b2e
kushb2 <kushkkr08@gmail.com>
2026-09-26 21:20:43 +0530

    Time: 2 ms (87.41%) | Memory: 43.9 MB (83.65%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -5,7 +5,7 @@ class Solution {
     };
 
     boolean valid(int[][] rooms, int i, int j , int n, int m){
-        return i>=0 && j >=0 && i < n && j < m && rooms[i][j] == 0;// fresh 
+        return i>=0 && j >=0 && i < n && j < m && rooms[i][j] == 1;// fresh 
     }
 
     public int orangesRotting(int[][] grid) {
@@ -15,46 +15,39 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 2){
-                    grid[i][j] = -2; // rotten
                     q.add(new int[] { i, j, 0});
-                }else if(grid[i][j] == 1){
-                    grid[i][j] = 0; // fresh
-                }else{
-                    grid[i][j] = -5;// empty
                 }
             }
         }
 
-
+        int timeTaken = 0;
         while(!q.isEmpty()){
             int[] gate = q.poll();
             int i = gate[0];
             int j = gate[1];
-            int distance = gate[2];
+            int time = gate[2];
 
             for(int[] it: dir){
                 int x = it[0] + i;
                 int y = it[1] + j;
 
                 if(valid(grid, x, y, n, m)){
-                    grid[x][y] = distance+1;
-                    q.add(new int[]{x,y,distance+1});
+                    timeTaken = Math.max(timeTaken, time+1);
+                    grid[x][y] = 2;
+                    q.add(new int[]{x,y,time+1});
                 }
             }
 
         }
-        int ans = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 0){
+                if(grid[i][j] == 1){
                     return -1;
-                }else if(grid[i][j] != -5){
-                    ans = Math.max(grid[i][j], ans);
-                }      
+                }     
             }
         }
 
-        return ans;
+        return timeTaken;
 
 
         
